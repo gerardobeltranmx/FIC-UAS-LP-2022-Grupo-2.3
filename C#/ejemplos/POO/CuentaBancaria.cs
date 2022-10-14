6d59bc78c6b759dcecdc7d132c3420e005be692f
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-14 16:40:13 +0000

    --

--- C#/ejemplos/POO/CuentaBancaria.cs
+++ C#/ejemplos/POO/CuentaBancaria.cs
@@ -1,14 +1,25 @@
+using System;
+class Programa {
+  static void Main() {
+
+    CuentaBancaria MiCuenta = new CuentaBancaria();
+
+    Console.WriteLine(MiCuenta.ObtenerSaldo());
+    
+  }
+}
+
 class CuentaBancaria
 {
-private decimal saldo;
-private string nombre;
-public void Retiro(decimal cantidad){
-saldo-=cantidad;
-}
-public void Deposito(decimal cantidad){
-saldo+=cantidad;
-}
-public decimal ObtenerSaldo( ){
-return saldo;
-}
+  private decimal saldo;
+  private string nombre;
+  public void Retiro(decimal cantidad){
+    saldo-=cantidad;
+  }
+  public void Deposito(decimal cantidad){
+    saldo+=cantidad;
+  }
+  public decimal ObtenerSaldo( ){
+    return saldo;
+  }
 }
