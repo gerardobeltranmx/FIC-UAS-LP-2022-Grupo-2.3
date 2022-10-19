c9370a18853e7181bde3645ec5f3ddcfad8b9cc6
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-19 16:29:27 +0000

    --

---
+++ C#/Programación Orientada a Objetos/Ejercicios/C. La esfera.cs
@@ -0,0 +1,34 @@
+using System;
+class Programa {
+  static void Main() {
+    Esfera Figura = new Figura();
+    double Radio;
+    Radio = double.Parse(Console.ReadLine());
+    Figura.CambiarRadio(Radio);
+    Console.WriteLine("Volumen:{0:F2} Superficie:{1:F2}",
+                Figura.Volumen(),
+                Figura.Superficie()
+              );
+  }
+}
+class Esfera{
+  private double Radio;
+  public Esfera (){
+    Radio = 0; 
+  }
+  public Esfera (double Radio){
+    this.Radio = Radio;
+  }
+  public double ObtenerRadio(){
+    return Radio;
+  }
+  public void CambiarRadio(double Radio){
+    this.Radio = Radio;
+  }
+  public double Superficie(){
+     return 4 * Math.PI * Math.Pow(Radio,2);   
+  }
+  public double Volumen(){
+     return 4.0/3.0 * Math.PI * Math.Pow(Radio,3);   
+  }
+}  
