8bbc5bcd23b6d8ea95219a38498216db440ef905
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-21 16:30:08 +0000

    --

---
+++ C#/Programación Orientada a Objetos/Ejemplos/cilindro.cs
@@ -0,0 +1,55 @@
+using System;
+class Programa {
+  static void Main() {
+
+    Cilindro Tanque = new  Cilindro();
+
+    Tanque.CambiarRadio(10);
+    Tanque.CambiarAltura(100);
+    Console.WriteLine("Radio: {0} Altura: {1} Volumen: {2} Area de la base: {3} Perimetro: {4}",
+                        Tanque.ObtenerRadio(),
+                        Tanque.ObtenerAltura(),
+                        Tanque.ObtenerVolumen(),
+                        Tanque.ObtenerArea(),
+                        Tanque.ObtenerPerimetro()
+                    );  
+
+  }
+}
+class Circulo{
+  private double radio;
+  public Circulo(int r){
+    radio=r;
+  }
+  public void CambiarRadio(double r){
+    radio=r;
+  }
+  public double ObtenerRadio(){
+    return radio;
+  }
+  public double ObtenerArea(){
+    return 3.1416*radio*radio;
+  }
+  public double ObtenerPerimetro(){
+    return 3.1416*2*radio;
+  }
+}
+
+class Cilindro:Circulo{
+  private double altura;
+  public Cilindro (){
+    
+  }
+  public Cilindro(double r, double h): base(r){
+    altura=h;
+  }
+  public void CambiarAltura(double h){
+    altura=h;
+  }
+  public double ObtenerAltura(){
+    return altura;
+  }
+  public double ObtenerVolumen(){
+    return ObtenerArea()*ObtenerAltura();
+  }
+}
